886313028032164c69db93ab8d46bc4e6a2cc371
HoangNguyen679 <ndhoang96@gmail.com>
2022-07-10 23:12:09 +0900

    add stories table example

---
+++ ruby/api/story_api.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require "#{File.dirname(__FILE__)}/../lib/dynamodb"
+
+# Service to scan all books table
+class StoryApi
+  TABLE_NAME = 'stories'
+
+  def initialize
+    @client = CustomDynamoDB.client
+  end
+
+  def describe_books_table()
+    @client.describe_table({ table_name: TABLE_NAME }).to_h
+  end
+
+  private
+
+  def benmark(description)
+    start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    response = yield
+    finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    puts('--------------------------------')
+    puts(description)
+    puts(finish - start)
+    response
+  end
+end
